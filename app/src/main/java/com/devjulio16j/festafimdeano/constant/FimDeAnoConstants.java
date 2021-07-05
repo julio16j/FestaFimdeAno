b1d6cdd7a74f3c5e5188b2c5cb3ee0b903d19be4
Julio Cesar Urbano Vasconcelos Filho <julio.filho@mv.com.br>
2021-07-05 00:20:43 -0300

    Festa Fim de Ano

---
+++ app/src/main/java/com/devjulio16j/festafimdeano/constant/FimDeAnoConstants.java
@@ -0,0 +1,8 @@
+package com.devjulio16j.festafimdeano.constant;
+
+public class FimDeAnoConstants {
+
+    public static String PRESENCE_KEY = "presenceKey";
+    public static String CONFIRMATION_YES = "Yes";
+    public static String CONFIRMATION_NO = "No";
+}
